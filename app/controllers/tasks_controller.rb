474432385e764e1475c1feaf18b71163d5ecdec4
rin <rin2486rin@gmail.com>
2020-10-28 11:01:58 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,33 +1,41 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy, :edit]
+    
   def index
     @tasks = Task.all
+    
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
-  def show
-  end
 
   def new
     @task = Task.new
   end
 
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
+    
     if @task.save
-      flash[:success] = 'タスクが正常に投稿されました'
-      redirect_to @task
+      flash[:success] = 'タスクを投稿しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'タスクが投稿されませんでした'
-      render :new
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'タスクの投稿に失敗しました。'
+      render 'tasks/index'
     end
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
-
+    
     if @task.update(task_params)
       flash[:success] = 'タスクは正常に更新されました'
       redirect_to @task
@@ -37,11 +45,11 @@ class TasksController < ApplicationController
     end
   end
 
+
   def destroy
     @task.destroy
-
-    flash[:success] = 'タスクは正常に削除されました'
-    redirect_to tasks_url
+    flash[:success] = 'タスクを削除しました。'
+    redirect_back(fallback_location: root_path)
   end
 
   private
@@ -51,7 +59,16 @@ class TasksController < ApplicationController
   end
   
   # Strong Parameter
+  
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
 
-  resources :tasks
+
+  root to: 'tasks#index'
+  
+  
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+  
+  resources :tasks, only: [:create, :destroy, :edit, :update, :show]
 end
