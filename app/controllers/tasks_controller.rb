233f8e45636c37db959d38b268f8297f41bccb16
rin <rin2486rin@gmail.com>
2020-10-29 14:07:08 +0000

    login-5th-kadai-commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,4 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy, :edit, :update, :show]
     
@@ -51,9 +50,6 @@ class TasksController < ApplicationController
 
   private
 
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   # Strong Parameter
   
